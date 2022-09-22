b6f3f29d4b027a4086dc199c1530264dc99cdc71
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-09-22 07:46:20 -0300

    v0.22.09.22

--- app/Views/Theme/Standard/ViewThList.php
+++ app/Views/Theme/Standard/ViewThList.php
@@ -1,8 +1,8 @@
-<a href="<?= PATH; ?>/th/<?= $id_th;?>" style="text-decoration: none;">
+<a href="<?= PATH; ?>/th/<?= $id_th; ?>" class="cards">
     <div class="card cards" style="width: 100%;">
-        <img src="<?=$img;?>" class="card-img-top" alt="top imagem">
-        <div class="card-body" style="background-color: #D8D8D8;">
-            <h5 class="card-title"><?= $th_name; ?></h5>
+        <img src="<?= $img; ?>" class="card-img-top p-3" alt="top imagem">
+        <div class="card-body cardsB">
+            <h5 class="card-title"><b><?= $th_name; ?></b></h5>
             <p class="card-text small"><?= $th_achronic; ?></p>
         </div>
     </div>
