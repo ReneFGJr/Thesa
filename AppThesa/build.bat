e3db30d92a2aec6288bcfa2658eb224af3158de9
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-04-21 10:14:23 -0300

    A

--- AppThesa/build.bat
+++ AppThesa/build.bat
@@ -1,6 +1,6 @@
 rem ng build --output-path dist --base-href './'.
 rem ng build --output-path dist --base-href '/'.
 rem ng build --output-path dist
-ng build --output-path=../web --base-href=/thesa/app/ --deploy-url=/thesa/app/
+ng build --output-path=../web --base-href=/thesa/web/ --deploy-url=/thesa/web/
 
 echo "Copiando arquivo"
