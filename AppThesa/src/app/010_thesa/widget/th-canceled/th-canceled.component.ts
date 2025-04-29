356cb081da34da2c8b5179c7f719eb02a90a0264
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2025-04-29 13:03:22 -0300

    Update th-canceled.component.ts

--- AppThesa/src/app/010_thesa/widget/th-canceled/th-canceled.component.ts
+++ AppThesa/src/app/010_thesa/widget/th-canceled/th-canceled.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-th-canceled',
   templateUrl: './th-canceled.component.html',
-  styleUrl: './th-canceled.component.scss'
+  styleUrl: './th-canceled.component.scss',
+  standalone: false,
 })
-export class ThCanceledComponent {
-
-}
+export class ThCanceledComponent {}
