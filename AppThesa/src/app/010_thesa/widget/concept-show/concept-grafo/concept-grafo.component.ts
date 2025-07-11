d9fd20cc8549b97fb39327e991aa2926131c698f
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2025-07-11 12:58:22 -0300

    Update

--- AppThesa/src/app/010_thesa/widget/concept-show/concept-grafo/concept-grafo.component.ts
+++ AppThesa/src/app/010_thesa/widget/concept-show/concept-grafo/concept-grafo.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Node, Edge } from '@swimlane/ngx-graph';
 
 @Component({
   selector: 'app-concept-grafo',
@@ -8,4 +9,47 @@ import { Component, Input } from '@angular/core';
 })
 export class ConceptGrafoComponent {
   @Input() conceptData: any;
+
+nodes: Node[] = [];
+  links: Edge[] = [];
+
+  ngOnInit(): void {
+    const concept = this.conceptData;
+
+    // Conceito principal
+    this.nodes.push({
+      id: concept.c_concept,
+      label: concept.prefLabel[0]?.Term,
+    });
+
+    // Conceitos mais amplos
+    for (let b of concept.broader) {
+      this.nodes.push({
+        id: 'B' + b.id,
+        label: b.Term,
+      });
+
+      this.links.push({
+        id: 'link-b-' + b.id,
+        source: 'B' + b.id,
+        target: concept.c_concept,
+        label: 'broader',
+      });
+    }
+
+    // Conceitos mais específicos
+    for (let n of concept.narrow) {
+      this.nodes.push({
+        id: 'N' + n.id,
+        label: n.Term,
+      });
+
+      this.links.push({
+        id: 'link-n-' + n.id,
+        source: concept.c_concept,
+        target: 'N' + n.id,
+        label: 'narrower',
+      });
+    }
+  }
 }
---
+++ node_modules/@angular/core/fesm2022/weak_ref.mjs
@@ -0,0 +1,12 @@
+/**
+ * @license Angular v20.1.0
+ * (c) 2010-2025 Google LLC. https://angular.io/
+ * License: MIT
+ */
+
+function setAlternateWeakRefImpl(impl) {
+    // TODO: remove this function
+}
+
+export { setAlternateWeakRefImpl };
+//# sourceMappingURL=weak_ref.mjs.map
