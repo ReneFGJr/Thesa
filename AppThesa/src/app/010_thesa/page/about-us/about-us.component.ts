759925f9d872ca90e3dd7dee9fd3754b55aecf0b
Rene Faustino Gabriel Junior <rene@sisdoc.com.br>
2026-09-21 18:45:37 -0300

    A

--- AppThesa/src/app/010_thesa/page/about-us/about-us.component.ts
+++ AppThesa/src/app/010_thesa/page/about-us/about-us.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { LanguageService } from '../../../000_core/service/language.service';
+import { aboutTranslations } from './about-translations';
 
 @Component({
   selector: 'app-about-us',
@@ -7,6 +9,8 @@ import { Component } from '@angular/core';
   styleUrl: './about-us.component.scss',
 })
 export class AboutUsComponent {
+  readonly language = inject(LanguageService);
+  readonly labels = computed(() => aboutTranslations[this.language.currentLanguage()]);
   version = 'v1.0';
   year = new Date().getFullYear();
 }
